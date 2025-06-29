284af19f4a6858063d882d5356066a18bcdb6ed9
Farzad Asgari <khufarzadasgari@gmail.com>
2025-06-30 01:46:04 +0330

    initializing index page

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+const Index = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold mb-4 text-blue-950">Welcome to WebADV</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Index;

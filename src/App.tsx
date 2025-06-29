ac7d529d9b72e8f27d3ba352210acd9e753df7a2
Farzad Asgari <khufarzadasgari@gmail.com>
2025-06-30 01:46:50 +0330

    initializing routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-// import React, {useState, useEffect} from 'react';
-import './index.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
 
 function App() {
     return (
-        <div className="flex h-screen items-center justify-center bg-gray-100">
-            <div className="bg-background text-primary rounded-lg p-6">
-                <h1 className="text-4xl font-bold">WebADV</h1>
-            </div>
+        <div className="min-h-screen">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Index/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }

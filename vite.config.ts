a3e17db517d212927a63ea5ce1c5003a27a1246e
Farzad Asgari <khufarzadasgari@gmail.com>
2025-07-02 21:35:32 +0330

    add note types

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
+import path from "path";
 
 export default defineConfig({
     base: '/webadv/',
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss()].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })

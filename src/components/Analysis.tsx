09bc9354348be2e53a8efb6db78176997d89f415
Farzad Asgari <khufarzadasgari@gmail.com>
2025-07-24 00:53:50 +0330

    initializing analysis tabs

--- src/components/Analysis.tsx
+++ src/components/Analysis.tsx
@@ -1,4 +1,5 @@
 import {Activity} from "lucide-react";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 
 export const Analysis = () => {
     return (
@@ -13,6 +14,25 @@ export const Analysis = () => {
                         Configure and apply advanced algorithms for cleaning and analyzing your ADV data
                     </p>
                 </div>
+                <Tabs defaultValue="pre" className="w-full">
+                    <TabsList className="grid w-full grid-cols-3 mb-8 h-12">
+                        <TabsTrigger value="pre" className="text-sm font-medium">Pre-Process</TabsTrigger>
+                        <TabsTrigger value="pro" className="text-sm font-medium">Despiking</TabsTrigger>
+                        <TabsTrigger value="pos" className="text-sm font-medium">Post-Process</TabsTrigger>
+                    </TabsList>
+
+                    <TabsContent value="pre" className="w-full">
+
+                    </TabsContent>
+
+                    <TabsContent value="pro" className="w-full">
+
+                    </TabsContent>
+
+                    <TabsContent value="pos" className="w-full">
+
+                    </TabsContent>
+                </Tabs>
             </div>
         </section>
     );
